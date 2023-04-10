3647228c99b6f85f43188f6697678f1625b332cf
wowba <leeyuwk54@gmail.com>
2023-04-11 00:33:33 +0900

    삭제 파일 반영

--- 1152.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs")
-const input = fs.readFileSync("input.txt").toString().split("\n")
-
-const wordArr = input[0].trim().split(" ")
-
-console.log(wordArr == "")
-console.log(wordArr.length)
--- 8393.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().split('\n')
-
-const num = Number(input[0])
-
-for (let i = 1; i <= 9; i++) {
-    console.log(num + " * " + i + " = " + (num * i))
-}
