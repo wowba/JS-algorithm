dd984c667d84fadebdeff17517365ba125835e79
wowba <leeyuwk54@gmail.com>
2023-04-17 23:41:07 +0900

    트리, 우선순위 큐

---
+++ 자료구조/queue.js
@@ -0,0 +1,35 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.headIndex]
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+
+while (queue.getLength() != 0) {
+  console.log(queue.dequeue())
+}
