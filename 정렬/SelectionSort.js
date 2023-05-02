d780b93096780774786d8b0c148241a29e66a1cc
wowba <leeyuwk54@gmail.com>
2023-05-03 00:31:32 +0900

    버블정렬 작성

--- 정렬/SelectionSort.js
+++ 정렬/SelectionSort.js
@@ -1,5 +1,4 @@
-const array = [1, 2, 4, 5, 7, 3, 6, 8, 9, 0]
-console.log(array)
+const array = [1, 5, 3, 4, 2]
 
 const SelectionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
@@ -13,8 +12,8 @@ const SelectionSort = (arr) => {
     let temp = arr[i]
     arr[i] = arr[minIndex]
     arr[minIndex] = temp
+    console.log(arr)
   }
-  console.log(arr)
 }
 
 SelectionSort(array)
