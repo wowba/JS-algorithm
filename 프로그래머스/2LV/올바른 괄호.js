57cd241357232994eed44f200bc4c156a80abb57
wowba <leeyuwk54@gmail.com>
2023-08-03 01:18:21 +0900

    올바른 괄호 문제풀이 추가

--- 프로그래머스/2LV/올바른 괄호.js
+++ 프로그래머스/2LV/올바른 괄호.js
@@ -1,3 +1,4 @@
+// 막무가내 문제풀이
 function solution(s){
     
   //     시간초과
@@ -30,3 +31,26 @@ function solution(s){
       }
       return answer
   }
+
+// 스택을 이용한 문제풀이
+function solution(s){
+  
+    let arr = []
+    let answer = true
+    for (const char of s) {
+        arr.push(char)
+        if (char === ")") {
+            if (arr.at(-2) === "(") {
+                arr.pop()
+                arr.pop()
+            } else {
+                answer = false
+            }
+        }
+    }
+    if (arr.at(-1) === "(") {
+        answer = false
+    }
+
+    return answer
+}
